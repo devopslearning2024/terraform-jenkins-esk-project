72dc88f65a796b87c4d3e75b026bdc8d73b04296
devopslearning2024 <159227248+devopslearning2024@users.noreply.github.com>
2024-03-27 20:16:22 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -49,8 +49,8 @@ module "eks" {
   eks_managed_node_groups = {
     nodes = {
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 1
+      desired_size = 1
 
       instance_types = ["t3.small"]
     }
